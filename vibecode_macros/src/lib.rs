c239e16047a9825db53146b5e8ced45171eafad3
Michael Barlow <michael@ramyun.co.uk>
2026-01-06 12:05:15 +0100

    underscore unused attr param

--- vibecode_macros/src/lib.rs
+++ vibecode_macros/src/lib.rs
@@ -4,7 +4,7 @@ use syn::{ItemFn, parse_macro_input};
 mod attribute;
 
 #[proc_macro_attribute]
-pub fn add(attr: TokenStream, annotated_item: TokenStream) -> TokenStream {
+pub fn add(_attr: TokenStream, annotated_item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(annotated_item as ItemFn);
 
     attribute::impl_add(&ast)
