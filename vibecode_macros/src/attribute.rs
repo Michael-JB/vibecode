56092df60620ff76bb24fcaffb4c5e6e95811dd9
Michael Barlow <michael@ramyun.co.uk>
2026-01-06 12:04:26 +0100

    parse add function from string
    an LLM would generate a string; this implementation is closer to that.

--- vibecode_macros/src/attribute.rs
+++ vibecode_macros/src/attribute.rs
@@ -3,10 +3,7 @@ use syn::ItemFn;
 
 pub fn impl_add(ast: &ItemFn) -> TokenStream {
     let fn_name = &ast.sig.ident;
-    let expanded = quote::quote! {
-        fn #fn_name(a: u64, b: u64) -> u64 {
-            a + b
-        }
-    };
-    TokenStream::from(expanded)
+    format!("fn {fn_name}(a: u64, b: u64) -> u64 {{ a + b }}")
+        .parse()
+        .expect("Failed to parse add function")
 }
