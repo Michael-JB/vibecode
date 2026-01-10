745f927ae41ebfcccd0d870764309fb52a661811
Michael Barlow <michael@ramyun.co.uk>
2026-01-10 13:27:05 +0100

    test: disable doc_markdown lint
    "OpenAI" in the README was tripping this up...

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(missing_docs)]
 #![warn(clippy::all, clippy::pedantic, clippy::cargo)]
+#![allow(clippy::doc_markdown)]
 #![doc = include_str!("../README.md")]
 
 pub use vibecode_macros::vibecode;
